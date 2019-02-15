6aeb21c75cd0c417a081295e776fd5893d879e27
zhenfan.yu <zhenfan.yu@mail.dealmoon.com>
2019-02-15 14:25:04 +0800

    feat-init

---
+++ config/config.default.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = appInfo => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1550211538264_5778';
+
+  // add your middleware config here
+  config.middleware = [];
+
+  // add your user config here
+  const userConfig = {
+    // myAppName: 'egg',
+  };
+
+  return {
+    ...config,
+    ...userConfig,
+  };
+};
