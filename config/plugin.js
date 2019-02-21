395d6c6544c5d98c6c1dfbb134db2b5dfa4ca1f6
zhenfan.yu <zhenfan.yu@mail.dealmoon.com>
2019-02-21 10:40:15 +0800

    fix-启动失败

--- config/plugin.js
+++ config/plugin.js
@@ -3,11 +3,6 @@
 // had enabled by egg
 // exports.static = true;
 
-exports.view = {
-    enable: true,
-    package: 'egg-view',
-};
-
 exports.ejs = {
     enable: true,
     package: 'egg-view-ejs',
